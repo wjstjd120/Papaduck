fd554d021facb64f7188df5d8c0048e9aa0782ed
LikelyLime <qorwlgml2003@naver.com>
2024-08-13 13:09:07 +0900

    art: #2 단어추가 페이지 UI 기본 만들기 및 CoreData 수정

---
+++ PapaDuck/Model/CoreDataClass/WordsEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WordsEntity+CoreDataClass.swift
+//  PapaDuck
+//
+//  Created by 백시훈 on 8/13/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WordsEntity)
+public class WordsEntity: NSManagedObject {
+
+}
