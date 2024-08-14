97d6572120f1938f1ac3af1a10baec4cab236fc2
LikelyLime <qorwlgml2003@naver.com>
2024-08-14 15:48:53 +0900

    bug: #5 단어 저장 수정

--- PapaDuck/Controller/CreateWordsController.swift
+++ PapaDuck/Controller/CreateWordsController.swift
@@ -21,25 +21,34 @@ class CreateWordsController: UIViewController{
     }
     /// 기본적으로 셋팅되는 단어장 저장하는 Action설정
     func setCreateWordsBookAction(){
-        createWordsView.saveButton.addTarget(self, action: #selector(saveBook), for: .touchDown)
+        createWordsView.saveButton.addTarget(self, action: #selector(save), for: .touchDown)
     }
     
-    /// 단어 저장을 위한 Action설정
-    func setCreateWordsAction(){
-        createWordsView.saveButton.removeTarget(nil, action: nil, for: .allEvents)
-        createWordsView.saveButton.addTarget(self, action: #selector(saveWord), for: .touchDown)
+    @objc func save(){
+        if createWordsView.wordsBookNameTextField.text!.isEmpty{
+            createWordsView.errorLabel.text = "필수 항목 입니다."
+            //self.createWordsView.wordsBookNameTextField.becomeFirstResponder()
+            return
+        }
+        saveCoreData()
     }
     
-    
-    /// 단어장을 저장하는 메서드
-    @objc func saveBook(){
-        bookCoreData.saveWordsBooks(wordsBookName: createWordsView.wordsBookNameTextField.text!, wordsExplain: createWordsView.explanationTextField.text ?? "")
+    /// 단어장UUID가 있을 경우에는 단어 저장 아니면 단어장 저장
+    @objc func saveCoreData() {
+        if let wordBookUUID = wordBookUUID {
+            // 단어 저장
+            wordCoreData.saveWords(wordsBookId: wordBookUUID, word: createWordsView.wordsBookNameTextField.text!, meaning: createWordsView.explanationTextField.text ?? "")
+            print("단어 저장됨")
+        } else {
+            // 단어장 저장
+            bookCoreData.saveWordsBooks(wordsBookName: createWordsView.wordsBookNameTextField.text!, wordsExplain: createWordsView.explanationTextField.text ?? "")
+            print("단어장 저장됨")
+        }
+        
+        navigationController?.popViewController(animated: true)
     }
     
-    /// 단어를 저장하는 메서드
-    @objc func saveWord(){
-        wordCoreData.saveWords(wordsBookId: wordBookUUID, word: createWordsView.wordsBookNameTextField.text!, meaning: createWordsView.explanationTextField.text ?? "")
-    }
+  
     
     /// 단어 저장하기 위해서 프로퍼티 변경
     /// - Parameters:
@@ -52,6 +61,5 @@ class CreateWordsController: UIViewController{
         createWordsView.explanationLabel.text = "뜻"
         createWordsView.explanationTextField.placeholder = "단어의 의미를 입력해주세요."
         wordBookUUID = wordBookId
-        setCreateWordsAction()
     }
 }
